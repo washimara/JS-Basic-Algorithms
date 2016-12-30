6ce614a22616c2b58f01a3a29508a31835c888ff
washimara <washimara@users.noreply.github.com>
2016-12-30 14:43:45 +0000

    Create FactorializeANumber.js

---
+++ FactorializeANumber.js
@@ -0,0 +1,20 @@
+function factorialize(num) {
+  
+  /*if(!num){ return 1;}
+  
+  var fact = num * (num-1);
+  var i = num - 1;
+  
+  for(i;i > 1;i--){
+    fact = fact * (num-i);
+  }
+  return fact;
+  */
+  
+  //Elegant Answer
+  
+  if (num === 0) { return 1; }
+  return num * factorialize(num-1);
+}
+
+factorialize(10);
